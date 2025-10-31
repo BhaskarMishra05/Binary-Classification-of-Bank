dcad783a82f3eb0cb2505914bf7203c21f27339a
BHaskarMishra05 <bhaskarmishra1590@gmail.com>
2025-10-31 13:21:21 +0530

    final dataset added

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -25,15 +25,18 @@ class DATA_INGESTION:
                     os.makedirs(os.path.dirname(self.data_ingestion_config.raw_data_path), exist_ok=True)
                     logging.info('Successfully created the directory for storing datasets')
                     logging.info('Reading the dataset')
-                    df = pd.read_csv(self.data_ingestion_config.raw_data_path, sep=';')
+                    df = pd.read_csv(self.data_ingestion_config.raw_data_path, sep=',')
                     df.columns = df.columns.str.replace('"','').str.strip()
-                    df['y']= df['y'].map({'yes':1, 'no': 0})
+                    if 'id' in df.columns:
+                           df = df.drop('id', axis=1)
+                    # new csv formate of target is already in binary 
+                    #df['y']= df['y'].map({'yes':1, 'no': 0})
                     logging.info(f"Cleaned columns: {df.columns.tolist()}")
                     logging.info('Splitting dataset in train and test set')
                     train_dataset, test_dataset = train_test_split(df, random_state=42, test_size= 0.2)
                     logging.info('Making the csv files of train and test datasets')
-                    train_dataset.to_csv(self.data_ingestion_config.train_data_path, index= False, sep= ',')
-                    test_dataset.to_csv(self.data_ingestion_config.test_data_path, index= False, sep= ',')
+                    train_dataset.to_csv(self.data_ingestion_config.train_data_path,index= False, sep= ',')
+                    test_dataset.to_csv(self.data_ingestion_config.test_data_path,index= False, sep= ',')
                     logging.info('Succefully executed the Data Ingestion stage')
                     return(
                             self.data_ingestion_config.raw_data_path,
